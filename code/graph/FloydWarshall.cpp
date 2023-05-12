9332c178ace3c28d9bd3be83581df1144c4a39b6
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 20:01:15 +0800

    Code Compression

--- code/graph/FloydWarshall.cpp
+++ code/graph/FloydWarshall.cpp
@@ -2,20 +2,15 @@ void init() {
     cin >> n >> m >> q;
     Each(i, dis) Each(j, i) j = LLINF;
 }
-
 while (m--) {
     int u, v; ll w;
     cin >> u >> v >> w;
     dis[u][v] = min(dis[u][v], w);
     dis[v][u] = min(dis[v][u], w);
 }
-
 void floydwarshall() {
 	FOR(k, 1, n+1, 1) {
 	    FOR(i, 1, n+1, 1) {
-	        FOR(j, 1, n+1, 1) {
-	            dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
-	        }
-	    }
-	}
-}
+		FOR(j, 1, n+1, 1) {
+		    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+} } } }
