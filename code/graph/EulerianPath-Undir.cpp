8f4c9568e2b8fecb8b8cdfa4fb0d6cbcaf7aa25e
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 20:00:39 +0800

    Code Compression

--- code/graph/EulerianPath-Undir.cpp
+++ code/graph/EulerianPath-Undir.cpp
@@ -1,12 +1,20 @@
+// from 1 to n
+#define gg return cout << "IMPOSSIBLE\n", void();
+
 int n, m;
 vector<int> g[maxn];
 bitset<maxn> inodd;
  
 void init() {
-    cin >> n >> m;
-    inodd.reset();
-}
- 
+cin >> n >> m;
+inodd.reset();
+for (int i = 0; i < m; i++) {
+	int u, v; cin >> u >> v;
+	inodd[u] = inodd[u] ^ true;
+	inodd[v] = inodd[v] ^ true;
+	g[u].emplace_back(v);
+	g[v].emplace_back(u);
+} }
 stack<int> stk;
 void dfs(int u) {
     while (!g[u].empty()) {
@@ -14,39 +22,4 @@ void dfs(int u) {
         g[u].pop_back();
         dfs(v);
     }
-    stk.push(u);
-}
- 
- 
-int main() {
-    WiwiHorz
-    init();
- 
-    REP(i, m) {
-        int u, v;
-        cin >> u >> v;
-        inodd[u] = inodd[u] ^ true;
-        inodd[v] = inodd[v] ^ true;
-        g[u].emplace_back(v);
-        g[v].emplace_back(u);
-    }
- 
-    FOR(i, 1, n+1, 1) {
-        if (inodd[i]) return cout << "IMPOSSIBLE\n", 0;
-    }
- 
-    dfs(1);
- 
-    FOR(i, 1, n+1, 1) {
-        if ((int)g[i].size()) return cout << "IMPOSSIBLE\n", 0;
-    }
- 
-    while (!stk.empty()) {
-        int u = stk.top();
-        stk.pop();
-        cout << u << ' ';
-    }
-    cout << endl;
- 
-    return 0;
-}
+stk.push(u);}
