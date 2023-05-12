e5b8bbabea83085d4095e6fe2a7409b4dc230cb6
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 18:01:23 +0800

    Code Compression

--- code/string/Zval.cpp
+++ code/string/Zval.cpp
@@ -1,20 +1,16 @@
 string is, it, s;
-int n;
-vector<int> z;
+int n; vector<int> z;
 void init() {
     cin >> is >> it;
     s = it+'0'+is;
     n = (int)s.size();
-    z.resize(n, 0);
-}
+    z.resize(n, 0); }
 void solve() {
-    int ans = 0;
-    z[0] = n;
+    int ans = 0; z[0] = n;
     for (int i = 1, l = 0, r = 0; i < n; i++) {
         if (i <= r) z[i] = min(z[i-l], r-i+1);
         while (i+z[i] < n && s[z[i]] == s[i+z[i]]) z[i]++;
         if (i+z[i]-1 > r) l = i, r = i+z[i]-1;
         if (z[i] == (int)it.size()) ans++;
     }
-    cout << ans << endl;
-}
+    cout << ans << endl; }
