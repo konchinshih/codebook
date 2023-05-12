bb38d73ed40de6e389f3e0b97ec07b57edffda95
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 18:42:56 +0800

    Code Compression

--- code/number-theory/MatrixFastPow.cpp
+++ code/number-theory/MatrixFastPow.cpp
@@ -1,38 +1,24 @@
 typedef vector<vector<ll> > mtx;
 typedef vector<ll> row;
- 
-int nr(mtx& a) {
-    return (int)a.size();
-}
-int nc(mtx& a) {
-    return (int)a[0].size();
-}
- 
-void mulip(mtx& a, mtx& b) { // multiply in-place
+inline int nr(mtx& a) {return (int)a.size();}
+int nc(mtx& a) {return (int)a[0].size();}
+void mul(mtx& a, mtx& b) { // multiply in-place
     mtx c = mtx(nr(a), row(nc(b), 0));
-    REP(i, nr(a)) {
-        REP(j, nc(b)) {
-            REP(k, nc(a)) {
-                c[i][j] += a[i][k] * b[k][j];
+    for (int i = 0; i < nr(a); i++)
+		for (int j = 0; j < nc(b); j++)
+			for (int k = 0; k < nc(a); k++) {
+				c[i][j] += a[i][k] * b[k][j];
                 if (c[i][j] >= MOD) c[i][j] %= MOD;
-            }
-        }
-    }
-    a = c;
-}
-
+			}
+a = c; }
 mtx pw(mtx& a, ll n) {
     mtx r = a;
-    REP(i, nr(a)) {
-        REP(j, nr(a)) {
-            r[i][j] = (i == j);
-        }
-    }
- 
+	for (int i = 0; i < nr(a); i++)
+		for (int j = 0; j < nr(a); j++)
+			r[i][j] = (i == j):
     while (n > 0) {
-        if (n & 1) mulip(r, a);
-        mulip(a, a);
+        if (n & 1) mul(r, a);
+        mul(a, a);
         n >>= 1;
     }
-    return r;
-}
+return r; }
