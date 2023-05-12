785d9281fc390785a6e3466179a8219d4b083079
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 19:57:56 +0800

    Code Compression

--- code/graph/EulerianPath-Dir.cpp
+++ code/graph/EulerianPath-Dir.cpp
@@ -1,15 +1,23 @@
 // from node 1 to node n
 #define gg return cout << "IMPOSSIBLE\n", 0
- 
+
 int n, m;
 vector<int> g[maxn];
 stack<int> stk;
 int in[maxn], out[maxn];
  
 void init() {
-    cin >> n >> m;
+cin >> n >> m;
+for (int i = 0; i < m; i++) {
+	int u, v; cin >> u >> v;
+	g[u].emplace_back(v);
+	out[u]++, in[v]++;
 }
- 
+for (int i = 1; i <= n; i++) {
+	if (i == 1 && out[i]-in[i] != 1) gg;
+	if (i == n && in[i]-out[i] != 1) gg;
+	if (i != 1 && i != n && in[i] != out[i]) gg;
+} }
 void dfs(int u) {
     while (!g[u].empty()) {
         int v = g[u].back();
@@ -18,36 +26,12 @@ void dfs(int u) {
     }
     stk.push(u);
 }
- 
- 
-int main() {
-    WiwiHorz
-    init();
- 
-    REP(i, m) {
-        int u, v;
-        cin >> u >> v;
-        g[u].emplace_back(v);
-        out[u]++, in[v]++;
-    }
- 
-    FOR(i, 1, n+1, 1) {
-        if (i == 1 && out[i]-in[i] != 1) gg;
-        if (i == n && in[i]-out[i] != 1) gg;
-        if (i != 1 && i != n && in[i] != out[i]) gg;
-    }
- 
-    dfs(1);
- 
-    FOR(i, 1, n+1, 1) {
+void solve() {
+	dfs(1)
+    for (int i = 1; i <= n; i++)
         if ((int)g[i].size()) gg;
-    }
- 
     while (!stk.empty()) {
         int u = stk.top();
         stk.pop();
         cout << u << ' ';
-    }
- 
-    return 0;
-}
+} }
