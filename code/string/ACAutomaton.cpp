5e2f13c6da4e06e3ec4a86a027432aac21d0dddd
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 18:34:39 +0800

    Code Compression

--- code/string/ACAutomaton.cpp
+++ code/string/ACAutomaton.cpp
@@ -1,18 +1,14 @@
 const int N = 1e3+5, MOD = 1e9+9;
- 
 int dp[N][N][15];
 map<char,int> m;
 int arr[N][4], fail[N], cnt[N], pos;
- 
 void insert(string &s){
 	int now = 0;
 	for(auto c : s){
 		int &to = arr[now][m[c]];
 		now = to ? to : to = ++pos;
 	}
-	cnt[now] = s.size();
-}
- 
+	cnt[now] = s.size(); }
 void build(){
 	queue<int> q;
 	for(int i = 0;i < 4;i++)
@@ -27,11 +23,7 @@ void build(){
 				while(tmp && !arr[tmp][i]) cnt[to] = max(cnt[to], cnt[arr[tmp][i]]),tmp = fail[tmp];
 				fail[to] = arr[tmp][i];
 				cnt[to] = max(cnt[to],cnt[arr[tmp][i]]);
-			}
-		}
-	}
-}
- 
+}}}}
 void match(string &s){
 	int now = 0;
 	for(int i = 0;i < s.size();i++){
@@ -40,5 +32,4 @@ void match(string &s){
 		if(cnt[now] > 0){
 			//FIND!
 		}
-	}
-}
+}}
