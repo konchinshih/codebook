cda3f919694503e48216f69983970e930c8831ed
Gino <53132291+Penguin-71630@users.noreply.github.com>
2023-05-12 19:15:48 +0800

    Code Compression

--- code/number-theory/PrimeSeive+Defactor.cpp
+++ code/number-theory/PrimeSeive+Defactor.cpp
@@ -1,30 +1,24 @@
 vector<int> lpf;
 vector<int> prime;
- 
 void seive() {
     lpf.resize(maxn, 1);
     for (int i = 2; i < maxn; i++) {
         if (lpf[i] == 1) {
             lpf[i] = i;
-            prime.eb(i);
+            prime.emplace_back(i);
         }
-        Each(j, prime) {
+        for (auto& j : prime) {
             if (i*j >= maxn) break;
             lpf[i*j] = j;
             if (j == lpf[i]) break;
-        }
-    }
-}
- 
+} } }
 vector<pii> fac;
 void defactor(int u) {
     fac.clear();
     while (u > 1) {
         int d = lpf[u];
-        fac.eb(mp(d, 0));
+        fac.emplace_back(mp(d, 0));
         while (u % d == 0) {
             u /= d;
             fac[(int)fac.size()-1].S++;
-        }
-    }
-}
+} } }
