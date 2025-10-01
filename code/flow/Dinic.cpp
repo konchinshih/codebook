61bb15e2d004e9af1df0e73c8c61bf102471f39d
Penguin-71630 <ginow514sm@gmail.com>
2025-10-01 12:44:35 +0800

    Added extensions for dinic and hopcroft-karp

--- code/flow/Dinic.cpp
+++ code/flow/Dinic.cpp
@@ -1,23 +1,35 @@
 // Author: Benson
 // Function: Max Flow, O(V^2 E)
+// Usage: Call init(n) first, then add(u, v, w) based on your model
+// (!) vertices 0-based
+// >>> flow() := return max flow
+// >>> find_cut() := return min cut + store cut set in dinic.cut
+// >>> find_matching() := return |M| + store matching plan in dinic.matching
+// >>> flow_decomposition := return max flow + store decomposition in dinic.D
+#define int long long
+#define eb emplace_back
+#define ALL(a) a.begin(), a.end()
 struct Dinic {
   struct Edge {
-    int t, c, r;
+    // t: to | C: original capacity | c: current capacity | r: residual edge | f: current flow
+    int t, C, c, r, f;
+    bool fw;  // is in forward-edge graph
     Edge() {}
-    Edge(int _t, int _c, int _r):
-      t(_t), c(_c), r(_r) {}
+    Edge(int _t, int _C, int _r, bool _fw, int _f=0):
+      t(_t), C(_C), c(_C), r(_r), fw(_fw), f(_f) {}
   };
   vector<vector<Edge>> G;
   vector<int> dis, iter;
-  int s, t;
-  void init(int n) {
+  int n, s, t;
+  void init(int _n) {
+    n = _n;
     G.resize(n), dis.resize(n), iter.resize(n);
     for(int i = 0; i < n; ++i)
       G[i].clear();
   }
   void add(int a, int b, int c) {
-    G[a].eb(b, c, G[b].size());
-    G[b].eb(a, 0, G[a].size() - 1);
+    G[a].eb(b, c, G[b].size(), true);
+    G[b].eb(a, 0, G[a].size() - 1, false);
   }
   bool bfs() {
     fill(ALL(dis), -1);
@@ -30,9 +42,7 @@ struct Dinic {
         if(e.c > 0 && dis[e.t] == -1) {
           dis[e.t] = dis[u] + 1;
           que.push(e.t);
-        }
-      }
-    }
+    } } }
     return dis[t] != -1;
   }
   int dfs(int u, int cur) {
@@ -45,12 +55,10 @@ struct Dinic {
           G[e.t][e.r].c += ans;
           e.c -= ans;
           return ans;
-        }
-      }
-    }
+    } } }
     return 0;
   }
-
+// find max flow
   int flow(int a, int b) {
     s = a, t = b;
     int ans = 0;
@@ -61,5 +69,78 @@ struct Dinic {
         ans += tmp;
     }
     return ans;
+  }
+// min cut plan
+  vector<pair<pair<int, int>, int>> cut;
+  int find_cut(int a, int b) {
+    int cut_sz = flow(a, b);
+    vector<int> vis(n, 0);
+    cut.clear();
+    function<void(int)> dfs = [&](int u) {
+      vis[u] = 1;
+      for (auto& e : G[u])
+        if (e.c > 0 && !vis[e.t])
+          dfs(e.t);
+    };
+    dfs(a);
+    for (int u = 0; u < n; u++)
+      if (vis[u])
+        for (auto& e : G[u])
+          if (!vis[e.t])
+            cut.eb(make_pair(make_pair(u, e.t), G[e.t][e.r].c));
+    return cut_sz;
+  }
+// bipartite matching plan
+  vector<pair<int, int>> matching;
+  int find_matching(int Xstart, int Xend, int Ystart, int Yend, int a, int b) {
+    int msz = flow(a, b);
+    matching.clear();
+    for (int x = Xstart; x <= Xend; x++)
+      for (auto& e : G[x])
+        if (e.c == 0 && Ystart <= e.t && e.t <= Yend)
+          matching.emplace_back(make_pair(x, e.t));
+    return msz;
+  }
+// flow decomposition
+  vector<pair<int, vector<int>>> D; // (flow amount, [path p1 ... pk])
+  int flow_decomposition(int a, int b) {
+    int mxflow = flow(a, b);
+
+    vector<vector<Edge>> fG(n); // graph consists of forward edges
+    for (int u = 0; u < n; u++) {
+      for (auto& e : G[u]) {
+        if (e.fw) {
+          e.f = e.C - e.c;
+          if (e.f > 0) fG[u].eb(e);
+    } } }
+
+    vector<int> vis;
+    function<int(int, int)> dfs = [&](int u, int cur) {
+      if (u == b) {
+        D.back().second.eb(u);
+        return cur;
+      }
+      vis[u] = 1;
+      for (auto& e : fG[u]) {
+        if (e.f > 0 && !vis[e.t]) {
+          int ans = dfs(e.t, min(cur, e.f));
+          if (ans > 0) {
+            e.f -= ans;
+            D.back().second.eb(u);
+            return ans;
+      } } }
+      return 0LL;
+    };
+    D.clear();
+    int quota = mxflow;
+    while (quota > 0) {
+      D.emplace_back(make_pair(0, vector<int>()));
+      vis.assign(n, 0);
+      int f = dfs(a, INF);
+      if (f == 0) break;
+      reverse(D.back().second.begin(), D.back().second.end());
+      D.back().first = f, quota -= f;
+    }
+    return mxflow;
   }
 };
--- code/flow/HopcroftKarp.cpp
+++ code/flow/HopcroftKarp.cpp
@@ -1,66 +1,86 @@
 // Author: Gino
 // Function: Max Bipartite Matching in O(V sqrt(E))
-// init() -> get() -> Ans = hk.MXCNT
+// Usage:
+// >>> init(nx, ny, m) -> add(x, y (+nx))
+// >>> hk.max_matching() := the matching plan stores in mx, my
+// >>> hk.min_vertex_cover() := the vertex cover plan stores in vcover
+// (!) vertices are 0-based: X = [0, nx), Y = [nx, nx+ny)
 struct HopcroftKarp {
-  // id: X = [1, nx], Y = [nx+1, nx+ny]
-  int n, nx, ny, m, MXCNT;
-  vector<vector<int> > g;
-  vector<int> mx, my, dis, vis;
-  void init(int nnx, int nny, int mm) {
-    nx = nnx, ny = nny, m = mm;
-    n = nx + ny + 1;
-    g.clear(); g.resize(n);
+  int n, nx, ny;
+  vector<vector<int> > G;
+  vector<int> mx, my;
+  void init(int _nx, int _ny) {
+    nx = _nx, ny = _ny;
+    n = nx + ny;
+    G.clear(); G.resize(n);
   }
   void add(int x, int y) {
-    g[x].emplace_back(y);
-    g[y].emplace_back(x);
+    G[x].emplace_back(y);
+    G[y].emplace_back(x);
   }
-  bool dfs(int x) {
-    vis[x] = true;
-    for (auto& y : g[x]) {
-      int px = my[y];
-      if (px == -1 ||
-          (dis[px] == dis[x]+1 &&
-           !vis[px] && dfs(px))) {
-        mx[x] = y;
-        my[y] = x;
-        return true;
-      }
-    }
-    return false;
-  }
-  void get() {
+  int max_matching() {
+    vector<int> dis, vis;
     mx.clear(); mx.resize(n, -1);
     my.clear(); my.resize(n, -1);
 
+    function<bool(int)> dfs = [&](int x) {
+      vis[x] = true;
+      for (auto& y : G[x]) {
+        int px = my[y];
+        if (px == -1 ||
+            (dis[px] == dis[x]+1 &&
+             !vis[px] && dfs(px))) {
+          mx[x] = y;
+          my[y] = x;
+          return true;
+      } }
+      return false;
+    };
+    
     while (true) {
       queue<int> q;
       dis.clear(); dis.resize(n, -1);
-      for (int x = 1; x <= nx; x++){
+      for (int x = 0; x < nx; x++){
         if (mx[x] == -1) {
           dis[x] = 0;
           q.push(x);
-        }
-      }
+      } }
       while (!q.empty()) {
         int x = q.front(); q.pop();
-        for (auto& y : g[x]) {
+        for (auto& y : G[x]) {
           if (my[y] != -1 && dis[my[y]] == -1) {
             dis[my[y]] = dis[x] + 1;
             q.push(my[y]);
-          }
-        }
-      }
-
+      } } }
       bool brk = true;
       vis.clear(); vis.resize(n, 0);
-      for (int x = 1; x <= nx; x++)
+      for (int x = 0; x < nx; x++)
         if (mx[x] == -1 && dfs(x))
           brk = false;
-
       if (brk) break;
     }
-    MXCNT = 0;
-    for (int x = 1; x <= nx; x++) if (mx[x] != -1) MXCNT++;
+    int ans = 0;
+    for (int x = 0; x < nx; x++) if (mx[x] != -1) ans++;
+    return ans;
+  }
+  vector<int> vcover;
+  int min_vertex_cover() {
+    int ans = max_matching();
+    vcover.clear();
+
+    vector<int> vis(n, 0);
+    function<void(int)> dfs = [&](int x) {
+      vis[x] = true;
+      for (auto& y : G[x]) {
+        if (y == mx[x] || my[y] == -1 || vis[y]) continue;
+        vis[y] = true;
+        dfs(my[y]);
+      }
+    };
+
+    for (int x = 0; x < nx; x++) if (mx[x] == -1) dfs(x);
+    for (int x = 0; x < nx; x++) if (!vis[x]) vcover.emplace_back(x);
+    for (int y = nx; y < nx + ny; y++) if (vis[y]) vcover.emplace_back(y);
+    return ans;
   }
 } hk;
