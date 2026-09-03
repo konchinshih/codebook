20322ca47baf4cdea93d609a794ec41af198cc5c
Yi-Ting Shih <ytshih@it.cs.nycu.edu.tw>
2026-09-04 06:24:36 +0800

    fix: align hash method in vimrc

--- hash.sh
+++ hash.sh
@@ -3,6 +3,12 @@ OUTPUT='hash.sha256'
 FILES="$(find "$SOURCEDIR" -type f)"
 
 while IFS='' read -r file; do
-  hval="$(openssl sha256 -r "$file" | cut -f1 -d' ')"
+  tmpfile="$(mktemp)"
+  if [[ "$file" == *.cpp ]]; then
+    cpp -dD -P < "$file" | tr -d '[:space:]' > "$tmpfile"
+  else
+    cp "$file" "$tmpfile"
+  fi
+  hval="$(openssl sha256 -r "$tmpfile" | cut -f1 -d' ')"
   printf '% 60s %s\n' "$file" "$hval"
 done <<< "$FILES" | tee "$OUTPUT"
