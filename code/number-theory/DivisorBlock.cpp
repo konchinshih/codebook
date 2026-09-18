7c3e335b0638846490d95f79473a2a218ae150a1
Gino <53132291+Penguin-71630@users.noreply.github.com>
2026-09-18 19:22:27 +0800

    Add Example
    Removed commented example usage from divisorBlock function.

--- code/number-theory/DivisorBlock.cpp
+++ code/number-theory/DivisorBlock.cpp
@@ -3,5 +3,9 @@ void divisorBlock(ll n, auto f) {
   for (ll l = 1, r, q; l <= n; l = r + 1) {
     q = n / l, r = n / q;
     f(l, r, q);
-  }
-}
+} }
+/* Example: sum of floor(n/i), i from 1 to n
+ll ans = 0;
+divisorBlock(n, [&](ll l, ll r, ll q) {
+  ans += q * (r - l + 1);
+}); */
