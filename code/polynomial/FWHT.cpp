2d3aa1cdcfd774750c85234c0ed995b8cbc409d5
Gino <53132291+Penguin-71630@users.noreply.github.com>
2026-09-18 19:25:31 +0800

    Refactor: sz(a) to a.size()

--- code/polynomial/FWHT.cpp
+++ code/polynomial/FWHT.cpp
@@ -2,10 +2,9 @@ using Poly = vector<ll>;
 
 // op: 0 = AND, 1 = OR, 2 = XOR; inv = inverse transform
 // conv: fwt(a,op), fwt(b,op), a[i] = mul(a[i], b[i]),
-// fwt(a,op,1)
-void fwt(Poly& a, int op, bool inv = false) {
-  int n = sz(a);
-  ll i2 = (mod + 1) / 2;
+// fwt(a,op,1)  => a is the answer
+void fwt(vector<ll>& a, int op, bool inv = false) {
+  int n = a.size(); ll i2 = (mod + 1) / 2;
   for (int s = 1; s < n; s <<= 1)
     for (int i = 0; i < n; i += s << 1)
       for (int j = i; j < i + s; j++) {
